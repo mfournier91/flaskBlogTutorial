c7c45a336d764c6a7c7b490239450925555a0294
Marc Fournier <marc.fournier91@gmail.com>
2017-02-05 21:15:55 -0500

    Jello, fakeuser: templates

--- app/views.py
+++ app/views.py
@@ -1,16 +1,11 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index() :
-    user = {'nickname': 'Miguel'}  # fake user
-    return '''
-    <html>
-      <head>
-        <title>Home Page</title>
-      </head>
-      <body>
-        <h1>Hello, ''' + user['nickname'] + '''</h1>
-      </body>
-    </html>
-    '''
+    user = {'nickname': 'Marc'}  # fake user
+    return render_template('index.html', title='Home', user=user)
+
+if __name__ == "__main__":
+    app.run()
